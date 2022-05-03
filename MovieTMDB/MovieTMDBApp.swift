ee0b138f1896ea81398f72dce932c1f70316f094
Lucas Gomes <lgomeszx@gmail.com>
2022-05-03 20:09:37 -0300

    Primeiro commit.

---
+++ MovieTMDB/MovieTMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTMDBApp.swift
+//  MovieTMDB
+//
+//  Created by Lucas Gomesx on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieTMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
