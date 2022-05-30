c9f559100f09f5d041e58edc2a420a396b5967a0
Lucas Gomes <lgomeszx@gmail.com>
2022-05-30 18:08:05 -0300

    Criado primeira tela da Datail view

--- MovieTMDB/MoviewDetailView/View/MovieDetailView.swift
+++ MovieTMDB/MoviewDetailView/View/MovieDetailView.swift
@@ -12,12 +12,29 @@ struct MovieDetailView: View {
     @ObservedObject var viewModel: MovieDetailViewModel
     
     var body: some View {
-        Text(viewModel.title)
+        ScrollView{
+            
+            VStack{
+                
+                Text(viewModel.title)
+                    .font(.title).bold()
+                
+                ImageView(url: viewModel.poster)
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity , maxHeight: 500)
+                
+                Text("Lancamento: " + viewModel.date)
+                
+                Text(viewModel.disc)
+                    .padding(20)
+                
+            }
+        }
     }
 }
 
 struct MovieDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        MovieDetailView(viewModel: MovieDetailViewModel(id: 10, title: "Miranha", poster: "", disc: "Teste disc", date: "11/11/11"))
+        MovieDetailView(viewModel: MovieDetailViewModel(id: 10, title: "Miranha", poster: "https://image.tmdb.org/t/p//w500/g4tMniKxol1TBJrHlAtiDjjlx4Q.jpg", disc: "Tefdasfasdfasdfasdfsadfsadfasdfdsfasdfasdfasdfasdfdsdafsdfasdfsadfsdfasdfsdfsafasdfadsste disc", date: "11/11/11"))
     }
 }
